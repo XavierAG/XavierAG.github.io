e0fa34124355f1b86117d9b2acde688e95cd959b
Xavier <xavierart2001@gmail.com>
2023-10-02 15:34:17 -0500

    added my pages

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,34 @@
 import Head from "next/head";
-import {BsFillMoonStarsFill} from 'react-icons/bs'
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub, AiFillMessage, AiOutlineMail} from 'react-icons/ai'
-import Image from "next/image"
-import deved from '../public/dev-ed-wave.png'
-import design from'../public/design.png';
-import code from'../public/code.png';
-import consulting from'../public/consulting.png';
-import web1 from "../public/web1.png"
-import web2 from "../public/web2.png"
-import web3 from "../public/web3.png"
-import web4 from "../public/web4.png"
-import web5 from "../public/web5.png"
-import web6 from "../public/web6.png"
-import { useState, useRef} from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillMessage,
+  AiOutlineMail,
+} from "react-icons/ai";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
+import biscordpage from "../public/biscordpage.png";
+import meetdown from "../public/meetdown.png";
+import { useState, useRef } from "react";
+import { pageExtensions } from "@/next.config";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
   const emailRef = useRef(null);
   const [copied, setCopied] = useState(false);
+  const history = useHistory();
 
   const copyEmailToClipboard = () => {
     // Select the email address text
     emailRef.current.select();
 
     // Copy the selected text to the clipboard
-    document.execCommand('copy');
+    document.execCommand("copy");
 
     // Deselect the text
     window.getSelection().removeAllRanges();
@@ -45,41 +49,80 @@ export default function Home() {
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-700">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons dark:text-white">developedby x</h1>
+            <h1 className="text-xl font-burtons dark:text-white">
+              developedby x
+            </h1>
             <ul className="flex items-center">
-              <li><BsFillMoonStarsFill onClick={()=> setDarkMode(!darkMode)} className="cursor-pointer text-xl"/></li>
-              <li><a className="bg-gradient-to-r from-cyan-300 to-cyan-500 dark:from-red-300 dark:to-red-500 text-white px-4 py-2 rounded-md ml-8" href="https://www.resume.lol/share/ha03v3f">Resume</a></li>
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-300 to-cyan-500 dark:from-red-300 dark:to-red-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="https://www.resume.lol/share/ha03v3f"
+                >
+                  Resume
+                </a>
+              </li>
             </ul>
           </nav>
           <div className=" text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-red-500">Xavier Guzman</h2>
-            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">Developer and designer.</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white">I am a Full Stack Developer with a passion for building beautiful and user-friendly websites. I have a strong unserstanding of both front-end and back-end development, and I am excited to put my skills to use in a professional setting. I am a quick learner and a team player, and I am confiudent that I can be a valuable asset to any web development team.</p>
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-red-500">
+              Xavier Guzman
+            </h2>
+            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
+              Developer and designer.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white">
+              I am a Full Stack Developer with a passion for building beautiful
+              and user-friendly websites. I have a strong unserstanding of both
+              front-end and back-end development, and I am excited to put my
+              skills to use in a professional setting. I am a quick learner and
+              a team player, and I am confiudent that I can be a valuable asset
+              to any web development team.
+            </p>
           </div>
           <div className=" text-5xl flex justify-center gap-16 py-3 text-gray-800 dark:text-white">
-            <a href="https://github.com/XavierAG"><AiFillGithub/></a>
-            <a href="https://www.linkedin.com/in/xavier-guzman/"><AiFillLinkedin/></a>
-            <a href="mailto:xavierart2001@gmail.com" onClick={copyEmailToClipboard}><AiOutlineMail/> <input
-              type="text"
-              ref={emailRef}
-              value="xavierart2001@gmail.com"
-              readOnly
-              style={{ position: 'absolute', opacity: 0, pointerEvents: 'none' }}
-          /></a>
+            <a href="https://github.com/XavierAG">
+              <AiFillGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/xavier-guzman/">
+              <AiFillLinkedin />
+            </a>
+            <a
+              href="mailto:xavierart2001@gmail.com"
+              onClick={copyEmailToClipboard}
+            >
+              <AiOutlineMail />{" "}
+              <input
+                type="text"
+                ref={emailRef}
+                value="xavierart2001@gmail.com"
+                readOnly
+                style={{
+                  position: "absolute",
+                  opacity: 0,
+                  pointerEvents: "none",
+                }}
+              />
+            </a>
           </div>
           <div className="flex mx-auto justify-center">
-            {copied && (
-              <span className="text-green-400">Email copied!</span>
-            )}
+            {copied && <span className="text-green-400">Email copied!</span>}
           </div>
           <div className=" relative mx-auto bg-gradient-to-b from-teal-500 dark:from-red-500 rounded-full w-80 h-80 my-20 overflow-hidden md:h-96 md:w-96">
-            <Image src={deved} layout="fill" objectFit="cover"/>
+            <Image src={deved} layout="fill" objectFit="cover" />
           </div>
         </section>
 
         <section>
           <div>
-            <h3 className="text-center text-3xl py-1 dark:text-red-500">My Skills</h3>
+            <h3 className="text-center text-3xl py-1 dark:text-red-500">
+              My Skills
+            </h3>
           </div>
           <div className="lg:flex lg:mx-auto justify-center gap-5">
             <div className="text-center shadow-lg p-10 rounded-xl my-10 flex-1 dark:bg-white">
@@ -122,16 +165,40 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className="text-center text-3xl py-1 dark:text-red-500">My Projects</h3>
+            <h3 className="text-center text-3xl py-1 dark:text-red-500">
+              My Projects
+            </h3>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 "><Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
-            <div className="basis-1/3 flex-1 "><Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
-            <div className="basis-1/3 flex-1 "><Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
-            <div className="basis-1/3 flex-1 "><Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
-            <div className="basis-1/3 flex-1 "><Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
-            <div className="basis-1/3 flex-1 "><Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
-           </div>
+            <a
+              href="https://meetupx.onrender.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="basis-1/3 flex-1 ">
+                <Image
+                  src={biscordpage}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                />
+              </div>
+            </a>
+            <a
+              href="https://meetupx.onrender.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="basis-1/3 flex-1 ">
+                <Image
+                  src={meetdown}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                />
+              </div>
+            </a>
+          </div>
         </section>
       </main>
     </div>
